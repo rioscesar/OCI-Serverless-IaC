8776566a589d0ef4d096543b71891d248d19d8f8
Cesar <rioscesar7@gmail.com>
2018-04-09 18:07:18 -0500

    error checking

--- eval/exec/func.py
+++ eval/exec/func.py
@@ -3,8 +3,8 @@ import json
 
 
 def handler(ctx, data=None, loop=None):
-    body = json.loads(data)
-    return eval(body)
+    body = json.loads(data) if len(data) > 0 else {"code": ""}
+    return eval(body["code"])
 
 
 if __name__ == "__main__":
