ca7f56b59ebcaea810d4c5fd243804bf525cbfcc
Cesar <rioscesar7@gmail.com>
2018-04-09 18:46:24 -0500

    body, code, and name

--- eval/exec/func.py
+++ eval/exec/func.py
@@ -3,8 +3,13 @@ import json
 
 
 def handler(ctx, data=None, loop=None):
-    body = json.loads(data) if "code" in data else {"code": ""}
-    return eval(str(body["code"]))
+    # serialize the string and then pass it into the json object
+    # should probably raise an error if name or code is not present
+    body = json.loads(data) if "code" in data and "name" in data else {"code": ""}
+    code = body.pop("code")
+    name = body.pop("name")
+    exec(code)
+    return name(**body)
 
 
 if __name__ == "__main__":
