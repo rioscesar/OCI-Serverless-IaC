bbcb615903f647d99dd0b18d3a9d148591b4c69d
Cesar <rioscesar7@gmail.com>
2018-04-10 13:34:50 -0500

    debug

--- eval/exec/func.py
+++ eval/exec/func.py
@@ -4,18 +4,19 @@ import logging
 import base64
 
 log = logging.getLogger(__name__)
-
+log.setLevel(logging.DEBUG)
+handler = logging.handlers.SysLogHandler(address = '/home/ubuntu')
+log.addHandler(handler)
 
 def handler(ctx, data=None, loop=None):
     # serialize the string and then pass it into the json object
     # should probably raise an error if name or code is not present
     body = json.loads(data) if "code" in data and "name" in data else {"code": ""}
-    log.warn(body.get("code"))
+    log.debug(body.get("code"))
     exec(body.pop("code"))
-    # c = """def func(a, b, c):\n return a+b+c"""
-    # exec(c)
+
     name = body.pop("name")
-    # name = "func"
+
     possibles = globals().copy()
     possibles.update(locals())
     method = possibles.get(name)
