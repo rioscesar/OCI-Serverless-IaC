1141a7acf7ab2dcf1d5ef4365b15f97696691010
Cesar <rioscesar7@gmail.com>
2018-04-09 19:07:14 -0500

    silly error

--- eval/exec/func.py
+++ eval/exec/func.py
@@ -6,14 +6,14 @@ def handler(ctx, data=None, loop=None):
     # serialize the string and then pass it into the json object
     # should probably raise an error if name or code is not present
     body = json.loads(data) if "code" in data and "name" in data else {"code": ""}
-    exec(d.pop("code"))
-    name = d.pop("name")
+    exec(body.pop("code"))
+    name = body.pop("name")
 
     possibles = globals().copy()
     possibles.update(locals())
     method = possibles.get(name)
 
-    return method(**d)
+    return method(**body)
 
 
 if __name__ == "__main__":
